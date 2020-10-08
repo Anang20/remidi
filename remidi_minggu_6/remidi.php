f16153d6a6010dc2fdf4dddccde9b05021aea353
Anang20 <anangsyah766@gmail.com>
2020-10-08 21:12:32 +0700

    update data array

--- remidi_minggu_6/remidi.php
+++ remidi_minggu_6/remidi.php
@@ -7,12 +7,18 @@ $hp = [
     ["Realme", "Realme 5i", 1650000],
     ["Realme", "Realme 6 Pro", 4000000],
     ["Realme", "Realme 5 Pro", 2999000],
+    ["Realme", "Realme 3", 2000000],
     ["Xiaomi", "Xiaomi Redmi Note 9 Pro", 3298000],
     ["Xiaomi", "Xiaomi Redmi Note 8 Pro", 3208000],
     ["Xiaomi", "Xiaomi Redmi 7a", 1300000],
     ["Vivo", "Vivo Y30", 2800000],
     ["Vivo", "Vivo Y20", 2200000],
-    ["Vivo", "Vivo Y11", 1800000]
+    ["Vivo", "Vivo Y11", 1800000],
+    ["Oppo", "Oppo A52", 2800000],
+    ["Oppo", "Oppo Reno3", 3480000],
+    ["Oppo", "Oppo A3S", 1000000],
+    ["Oppo", "Oppo A12", 1500000]
+
 ];
 
 // Membuat Pilihan
